90402ed9fc42ba55828295ae4fa99a03279c5a1d
Станислав Шубин <sti.99@mail.ru>
2021-12-20 11:56:44 +0300

    add some initial trash and made SetWindowSize better

--- console/console.cpp
+++ console/console.cpp
@@ -2,30 +2,24 @@
 using namespace std;
 #include <windows.h>
 
-inline void SetWindowSize(const short w,const short h)
+void SetWindowSize(short w,short h)
 {
-	_COORD coord;
-	coord.X=w;
-	coord.Y=h;
-	_SMALL_RECT rect;
-	rect.Top=0;
-	rect.Left=0;
-	rect.Bottom=h-1;
-	rect.Right=w-1;
+	w--;h--;
 	HANDLE handle=GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleScreenBufferSize(handle,coord);
-	SetConsoleWindowInfo(handle,true,&rect);
+	SetConsoleScreenBufferSize(handle,{0,0});
+	SetConsoleWindowInfo(handle,true,new _SMALL_RECT{0,0,w,h});
 }
 
 int main()
 {
     const short w=16;
     const short h=16;
+	
 	SetWindowSize(w,h);
     const size_t wh=w*h;
 
     char screen[wh+1];
-    memset(screen,'.',wh);
+    for(size_t i=0;i<wh;i++)screen[i]=rand()%100+32;
 
 	HANDLE console=CreateConsoleScreenBuffer(GENERIC_READ|GENERIC_WRITE,0,0,CONSOLE_TEXTMODE_BUFFER,0);
 	SetConsoleActiveScreenBuffer(console);
