f1b2909a83b9f5c279f2ad551400d443c888028a
Станислав Шубин <sti.99@mail.ru>
2021-12-20 14:56:09 +0300

    fix 60 fps

--- console/console.cpp
+++ console/console.cpp
@@ -1,11 +1,11 @@
-﻿#include <iostream>
-using namespace std;
-#include <windows.h>
+﻿//#include <iostream>
+//using namespace std;
+#include <windows.h>//for win api
+#include <time.h>//for fix fps
 
-inline void SetWindowSize(short w,short h)
+inline void SetWindowSize(const short w,const short h)
 {
-	w--;h--;
-	HANDLE handle=GetStdHandle(STD_OUTPUT_HANDLE);
+	const HANDLE handle=GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleScreenBufferSize(handle,{0,0});
 	SetConsoleWindowInfo(handle,true,new _SMALL_RECT{0,0,w,h});
 }
@@ -14,14 +14,13 @@ int main()
 {
     const short w=16;
     const short h=16;
-	
-	SetWindowSize(w,h);
+	SetWindowSize(w-1,h-1);
     const size_t wh=w*h;
 
-    char screen[wh+1];
-    for(size_t i=0;i<wh;i++)screen[i]=rand()%100+32;
+    char screen[wh+1];//an array that will be puted on the screen
+    for(size_t i=0;i<wh;i++)screen[i]=rand()%100+32;//fill the array with some trash
 
-	HANDLE console=CreateConsoleScreenBuffer(GENERIC_READ|GENERIC_WRITE,0,0,CONSOLE_TEXTMODE_BUFFER,0);
+	const HANDLE console=CreateConsoleScreenBuffer(GENERIC_READ|GENERIC_WRITE,0,0,CONSOLE_TEXTMODE_BUFFER,0);
 	SetConsoleActiveScreenBuffer(console);
 
 	screen[0]='@';
@@ -31,7 +30,9 @@ int main()
 	{
 		screen[i]='.'; screen[i+1]='@';
 		WriteConsoleOutputCharacterA(console,screen,wh,{0,0},&count);
-		Sleep(1);
+
+		const size_t dt=CLOCKS_PER_SEC/60;
+		for(size_t t=clock();(clock()-t)<dt;);
 	}
 
     return 0;
